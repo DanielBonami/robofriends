90ca994e42334bbb62b32bfbdf63f398bd4760e1
DanielBonami <bonamidaniel4@gmail.com>
2024-12-30 17:43:36 -0600

    fixing security issues updated libraries

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import Card from './Card';
+import Card from './card';
+const cardList = ({ robots }) => {
+    return ( 
+      <div>
+         {
+            robots.map((user, i) => {
+              return (
+                <card 
+                 key={i}
+                 id={robots[i].id}
+                 name={robots[i].name}
+                 email={robots[i].email}
+                />
+              );
+            })
+         }
+      </div>  
 
-const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
-  );
+    );
 }
 
-export default CardList;
+export default cardList;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
-// import ReactDOM from 'react-dom'; The new way to import createRoot:
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom';
 import './index.css'; 
 import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-// import registerServiceWorker from './registerServiceWorker';
 
-
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+ReactDOM.render(<App /> , document.getElementById('root'));
+registerServiceWorker();
